bbd37b925755073e4c3e2d1bf127f2c9b124e2c0
Aarjugit <aarjugoswami19@gmail.com>
2026-01-06 21:34:02 +0530

    recommendation based program

---
+++ test.py
@@ -0,0 +1,15 @@
+# Write Python 3 code in this online editor and run it.
+# count = 0
+# n=5873
+# while n > 0:
+#     count+=1
+#     n = n // 10
+# print(count)
+
+# TC=0(LOG10(N))
+# from math import *
+# def count_digit(num):
+#     return int(log10(num)+1)
+# print(count_digit(5873))
+
+#check number will be palindrome or not
